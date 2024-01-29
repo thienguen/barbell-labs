b2a3a0c45e9169c273c37438b3e08681d6bda3e0
Elliot Wesoff <ewesoff@gmail.com>
2024-01-29 09:20:32 -0800

    tiny scatterplot refactor

--- repo_mining/elliot_scatterplot.py
+++ repo_mining/elliot_scatterplot.py
@@ -5,17 +5,16 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 authors = {}
-filenames = []
 marker_size = 10
 
 with open('data/scatterplot_data.json', 'r') as f:
     scatterplot_data = json.loads(f.read())
 
-for filename, author_data in scatterplot_data.items():
-    if filename not in filenames:
-        filenames.append(filename)
+filenames = list(scatterplot_data.keys())
 
-    filename_i = filenames.index(filename)
+for i in range(len(filenames)):
+    filename = filenames[i]
+    author_data = scatterplot_data[filename]
 
     for author, weeks in author_data.items():
         if author not in authors:
@@ -25,7 +24,7 @@ for filename, author_data in scatterplot_data.items():
             authors[author] = color
         
         for week in weeks:
-            plt.scatter(filename_i, week, color=authors[author], s=marker_size)
+            plt.scatter(i, week, color=authors[author], s=marker_size)
 
 plt.legend = [mpatches.Patch(color=color, label=author) for author, color in authors.items()]
 plt.xlabel('file')
