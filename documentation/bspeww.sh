e1d6a57eef1ee97019edf32bbc8cd7548d3163ce
Elliot Wesoff <ewesoff@gmail.com>
2024-02-14 21:25:36 -0800

    Revert "add documentation folder and bspeww.sh"
    This reverts commit 88b4cbcde14f2c2e62401b9520a3a240ae3b7a99.

--- documentation/bspeww.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-  bspc wm -d \
-    | jq -c '.monitors[0].desktops[] | .root // {} | [.[] | recurse | select(.instanceName?) | .instanceName] | join("; ")' \
-    | jq -c -s . \
-    >> ~/tmp/bspeww/desktops/data
