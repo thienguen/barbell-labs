53ff3dfd2cbf5e3493de96d09dbf80026aac54fc
mva919 <mva919@gmail.com>
2024-02-25 13:24:30 -0800

    File to add documentation for task 2.2 of ChatGPT lab

---
+++ code_snippets/Marcos_useOutsideClickDetector.tsx
@@ -0,0 +1,22 @@
+import { type RefObject, useEffect } from "react";
+
+const useOutsideClickDetecter = (
+  ref: RefObject<HTMLDivElement>,
+  callback: () => void
+) => {
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, callback]);
+};
+
+export default useOutsideClickDetecter;
