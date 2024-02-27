2925b12b2a471db4075eefebe96237deb65f68ec
Daniel <daniawesomecool@gmail.com>
2024-02-26 21:04:07 -0800

    Added extra documentation for the math portion of the code

--- code_snippets/daniel_documentation.cpp
+++ code_snippets/daniel_documentation.cpp
@@ -57,6 +57,17 @@ Pseudocode:
    b. Calculate sum of multiples of 3 and 5 below 'n' using formulas.
    c. Output the sum.
 
+Math Forumulas:
+1. Find the number of multiples of 3 or 5 below 'n' - 1.
+    (n-1)/3 = number of multiples of 3 below n
+    (n-1)/5 = number of multiples of 5 below n
+2. Calculate the sum of multiples of 3 or 5 using the formula for sum of an arithmetic series (Little Guass formula)
+    sum = (n * (n + 1))/2
+3. Adjust for multiples of both 3 and 5 (15) by subtracting the sum of multiples of 15.
+    find the number of multiples of 15 below n - 1
+        (n-1)/15 = number of multiples of 15 below n
+    sum -= (15 * temp * (temp + 1))/2 in order to exclude the sum of multiples of 15.
+
 */
 
 #include <iostream>
