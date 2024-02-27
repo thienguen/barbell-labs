319f11a198d64f54b9ed4fba17be29c17eebdbf0
AaronFmyHub <146596967+AaronFmyHub@users.noreply.github.com>
2024-02-26 21:06:48 -0800

    Create Yidong_Refactoring.java

---
+++ code_snippets/Yidong_Refactoring.java
@@ -0,0 +1,12 @@
+// Original Code Snippet
+public boolean isSomething()
+{
+    if (!this.calculateSomeBooleanValue())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
